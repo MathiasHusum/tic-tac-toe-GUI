3a064acd9594899a11d1885c415405c5cca01f19
soipi <soipi@DESKTOP-8M1B4KG>
2016-10-12 12:38:33 +0200

    Add project files.

---
+++ tic tac toe thing/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace tic_tac_toe_thing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.a1 = new System.Windows.Forms.Button();
+            this.a2 = new System.Windows.Forms.Button();
+            this.a3 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.c1 = new System.Windows.Forms.Button();
+            this.c2 = new System.Windows.Forms.Button();
+            this.c3 = new System.Windows.Forms.Button();
+            this.restartButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.player1Name = new System.Windows.Forms.Label();
+            this.player2Name = new System.Windows.Forms.Label();
+            this.scorePlayer1 = new System.Windows.Forms.Label();
+            this.scorePlayer2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // a1
+            // 
+            this.a1.BackColor = System.Drawing.Color.Red;
+            this.a1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a1.Location = new System.Drawing.Point(225, 98);
+            this.a1.Name = "a1";
+            this.a1.Size = new System.Drawing.Size(100, 100);
+            this.a1.TabIndex = 0;
+            this.a1.Text = "A1";
+            this.a1.UseVisualStyleBackColor = false;
+            // 
+            // a2
+            // 
+            this.a2.BackColor = System.Drawing.Color.Green;
+            this.a2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a2.Location = new System.Drawing.Point(331, 98);
+            this.a2.Name = "a2";
+            this.a2.Size = new System.Drawing.Size(100, 100);
+            this.a2.TabIndex = 1;
+            this.a2.Text = "A2";
+            this.a2.UseVisualStyleBackColor = false;
+            // 
+            // a3
+            // 
+            this.a3.BackColor = System.Drawing.Color.Blue;
+            this.a3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a3.Location = new System.Drawing.Point(437, 98);
+            this.a3.Name = "a3";
+            this.a3.Size = new System.Drawing.Size(100, 100);
+            this.a3.TabIndex = 2;
+            this.a3.Text = "A3";
+            this.a3.UseVisualStyleBackColor = false;
+            // 
+            // b1
+            // 
+            this.b1.BackColor = System.Drawing.Color.Red;
+            this.b1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b1.Location = new System.Drawing.Point(225, 204);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(100, 100);
+            this.b1.TabIndex = 3;
+            this.b1.Text = "B1";
+            this.b1.UseVisualStyleBackColor = false;
+            // 
+            // b2
+            // 
+            this.b2.BackColor = System.Drawing.Color.Green;
+            this.b2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b2.Location = new System.Drawing.Point(331, 204);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(100, 100);
+            this.b2.TabIndex = 4;
+            this.b2.Text = "B2";
+            this.b2.UseVisualStyleBackColor = false;
+            // 
+            // b3
+            // 
+            this.b3.BackColor = System.Drawing.Color.Blue;
+            this.b3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b3.Location = new System.Drawing.Point(437, 204);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(100, 100);
+            this.b3.TabIndex = 5;
+            this.b3.Text = "B3";
+            this.b3.UseVisualStyleBackColor = false;
+            // 
+            // c1
+            // 
+            this.c1.BackColor = System.Drawing.Color.Red;
+            this.c1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c1.Location = new System.Drawing.Point(225, 310);
+            this.c1.Name = "c1";
+            this.c1.Size = new System.Drawing.Size(100, 100);
+            this.c1.TabIndex = 6;
+            this.c1.Text = "C1";
+            this.c1.UseVisualStyleBackColor = false;
+            // 
+            // c2
+            // 
+            this.c2.BackColor = System.Drawing.Color.Green;
+            this.c2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c2.Location = new System.Drawing.Point(331, 310);
+            this.c2.Name = "c2";
+            this.c2.Size = new System.Drawing.Size(100, 100);
+            this.c2.TabIndex = 7;
+            this.c2.Text = "C2";
+            this.c2.UseVisualStyleBackColor = false;
+            // 
+            // c3
+            // 
+            this.c3.BackColor = System.Drawing.Color.Blue;
+            this.c3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c3.Location = new System.Drawing.Point(437, 310);
+            this.c3.Name = "c3";
+            this.c3.Size = new System.Drawing.Size(100, 100);
+            this.c3.TabIndex = 8;
+            this.c3.Text = "C3";
+            this.c3.UseVisualStyleBackColor = false;
+            // 
+            // restartButton
+            // 
+            this.restartButton.BackColor = System.Drawing.Color.Orange;
+            this.restartButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.restartButton.Location = new System.Drawing.Point(12, 12);
+            this.restartButton.Name = "restartButton";
+            this.restartButton.Size = new System.Drawing.Size(129, 59);
+            this.restartButton.TabIndex = 9;
+            this.restartButton.Text = "Restart";
+            this.restartButton.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(174, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 38);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "TicTacToe";
+            // 
+            // player1Name
+            // 
+            this.player1Name.AutoSize = true;
+            this.player1Name.Location = new System.Drawing.Point(9, 105);
+            this.player1Name.Name = "player1Name";
+            this.player1Name.Size = new System.Drawing.Size(45, 13);
+            this.player1Name.TabIndex = 12;
+            this.player1Name.Text = "Player 1";
+            // 
+            // player2Name
+            // 
+            this.player2Name.AutoSize = true;
+            this.player2Name.Location = new System.Drawing.Point(9, 181);
+            this.player2Name.Name = "player2Name";
+            this.player2Name.Size = new System.Drawing.Size(45, 13);
+            this.player2Name.TabIndex = 13;
+            this.player2Name.Text = "Player 2";
+            // 
+            // scorePlayer1
+            // 
+            this.scorePlayer1.AutoSize = true;
+            this.scorePlayer1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scorePlayer1.Location = new System.Drawing.Point(19, 134);
+            this.scorePlayer1.Name = "scorePlayer1";
+            this.scorePlayer1.Size = new System.Drawing.Size(122, 24);
+            this.scorePlayer1.TabIndex = 14;
+            this.scorePlayer1.Text = "ScorePlayer1";
+            // 
+            // scorePlayer2
+            // 
+            this.scorePlayer2.AutoSize = true;
+            this.scorePlayer2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scorePlayer2.Location = new System.Drawing.Point(19, 216);
+            this.scorePlayer2.Name = "scorePlayer2";
+            this.scorePlayer2.Size = new System.Drawing.Size(122, 24);
+            this.scorePlayer2.TabIndex = 15;
+            this.scorePlayer2.Text = "ScorePlayer2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(641, 466);
+            this.Controls.Add(this.scorePlayer2);
+            this.Controls.Add(this.scorePlayer1);
+            this.Controls.Add(this.player2Name);
+            this.Controls.Add(this.player1Name);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.restartButton);
+            this.Controls.Add(this.c3);
+            this.Controls.Add(this.c2);
+            this.Controls.Add(this.c1);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.a3);
+            this.Controls.Add(this.a2);
+            this.Controls.Add(this.a1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe - Gruppe 8";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button a1;
+        private System.Windows.Forms.Button a2;
+        private System.Windows.Forms.Button a3;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button c1;
+        private System.Windows.Forms.Button c2;
+        private System.Windows.Forms.Button c3;
+        private System.Windows.Forms.Button restartButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label player1Name;
+        private System.Windows.Forms.Label player2Name;
+        private System.Windows.Forms.Label scorePlayer1;
+        private System.Windows.Forms.Label scorePlayer2;
+    }
+}
+
